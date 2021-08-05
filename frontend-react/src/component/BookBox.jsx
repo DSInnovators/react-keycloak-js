b2a6dabd6c3723aed7af6f25b098ae9b132fabb3
SyedAkash <mahmud.hasan@dsinnovators.com>
2021-08-05 15:20:17 +0600

    Not working client account linking

--- frontend-react/src/component/BookBox.jsx
+++ frontend-react/src/component/BookBox.jsx
@@ -7,6 +7,10 @@ import HttpService from "../services/HttpService";
 import Akash from "./akash";
 import jwt_decode from "jwt-decode";
 
+import { sha256, sha224 } from 'js-sha256';
+import {decode as base64_decode, encode as base64_encode} from 'base-64';
+import { v4 as uuidv4 } from 'uuid';
+
 const LIST_STUDENTS = 'http://localhost:7070/user/hello';
 const LIST_ADMINS = 'http://localhost:7070/admin/hello';
 
@@ -15,7 +19,48 @@ const Welcome = () => {
 
     useEffect(() => {
 
-        console.log(JSON.stringify(jwt_decode(UserService.getToken())))
+
+       // const  clientId = jwt_decode(UserService.getToken()).azp;
+        const  clientId = 'dhaka-client';
+        console.log('Clientid',clientId);
+
+        //const nonce = uuidv4();
+        const nonce = jwt_decode(UserService.getToken()).nonce;
+        console.log('nonce',nonce);
+
+        //session_state
+        const  session_state = jwt_decode(UserService.getToken()).session_state;
+        console.log('session_state',session_state);
+
+        const  provider = 'keycloak-dhaka';
+        console.log('provider',provider);
+
+        const input = nonce + session_state + clientId + provider;
+        console.log('input',input);
+       // console.log(JSON.stringify(jwt_decode(UserService.getToken())))
+
+
+        const messageDigest = sha256(input);
+        console.log('Message digest',messageDigest);
+
+
+        let encoded = base64_encode(messageDigest);
+        console.log('Base64 encoded',encoded);
+
+        //let redirect_uri = 'http://192.168.31.80:3000';
+        let redirect_uri = 'http%3A%2F%2F192.168.31.80%3A3000';
+        console.log('redirect_uri',redirect_uri);
+
+        const query_param = 'client_id=' +clientId +'&redirect_uri='+redirect_uri + '&nonce=' + nonce +'&hash='+messageDigest;
+
+        //http://localhost:8000/auth/realms/BANBEIS-BROKER/broker/keycloak-dhaka/link?client_id=dhaka-client&redirect_uri=http%3A%2F%2F192.168.31.80%3A3000&nonce=84143699-9f3f-4661-ac3e-e029d450f2fa&hash=b103d71e9d5d121af5025a290f3e08c3152adaafc964b03afe6711ad9ce23fd2
+        // /{auth-server-root}/auth/realms/{realm}/broker/{provider}/link?client_id={id}&redirect_uri={uri}&nonce={nonce}&hash={hash}
+        const url = 'http://localhost:8000/auth/realms/BANBEIS-BROKER/broker/keycloak-dhaka/link' + "?" + query_param ;
+        console.log('url',url);
+
+        window.location.replace(url)
+
+
     }, []);
 
     const getUserData = async ()=>{
--- frontend-react/src/component/Welcome.jsx
+++ frontend-react/src/component/Welcome.jsx
@@ -1,6 +1,26 @@
 import UserService from "../services/UserService";
+import { useEffect } from "react";
+import { sha256, sha224 } from 'js-sha256';
+import {decode as base64_decode, encode as base64_encode} from 'base-64';
+import { v4 as uuidv4 } from 'uuid';
 
-const Welcome = () => (
+
+
+const Welcome = () => {
+    useEffect(() => {
+
+        const messageDigest = sha256('The quick brown fox jumps over the lazy dog');
+        console.log('Message digest',messageDigest);
+
+        const input = "akash";
+        let encoded = base64_encode(input);
+        console.log('Base64 encoded',encoded);
+
+        const nonce = uuidv4();
+        console.log('nonce',nonce);
+
+    }, []);
+    return (
   <div className="jumbotron">
     <h1>Hello Anonymous!</h1>
     <p className="lead">Please authenticate yourself!</p>
@@ -8,6 +28,6 @@ const Welcome = () => (
       <button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>Login</button>
     </p>
   </div>
-)
+)}
 
 export default Welcome
